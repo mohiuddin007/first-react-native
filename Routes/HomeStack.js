239a43014ff323ccb798dca26fd45e9ad074511e
webdevelopermohiuddin <mohiuddinmazumder94@gmail.com>
2021-03-09 01:22:35 +0600

    navigation problem

--- Routes/HomeStack.js
+++ Routes/HomeStack.js
@@ -1,21 +1,31 @@
 import { createStackNavigator } from 'react-navigation-stack';
 import WelcomeScreen from '../components/Screens/WelcomeScreen';
 import ViewImageScreen from '../components/Screens/ViewImageScreen';
+import Header from '../Shared/header';
 
 const screens = {
     
       Home: {
           screen: WelcomeScreen,
           navigationOptions: {
-              title: 'Welcome',
-            //   headerStyle: {background: '#eee'}
+              headerTitle: () => {
+                return (
+                      <Header/>
+                //     <View style={styles.header}>
+                //     <MaterialIcons name="menu" size={28} onPress={openMenu} style={styles.icon}/>
+                //    <View>
+                //        <Text style={styles.headerText}>Game Center</Text>
+                //    </View>
+                // </View>
+                )
+
+              }
           }
       },
       Details: {
         screen: ViewImageScreen,
         navigationOptions: {
             title: 'Details',
-            // headerStyle: {background: '#eee'}
         }
     },
 }
